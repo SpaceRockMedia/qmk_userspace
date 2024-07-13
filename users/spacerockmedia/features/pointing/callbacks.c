7dda98363b2fbc2ab3080313087d1bb902c30b91
Shawn McElroy <shawn@spacerockmedia.com>
2024-07-13 13:12:53 -0700

    more cleanup

--- users/spacerockmedia/features/pointing/callbacks.c
+++ users/spacerockmedia/features/pointing/callbacks.c
@@ -1,4 +1,6 @@
-// in keymap.c:
-void pointing_device_init_user(void) {
-    set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
-}
+
+#include "features/pointing/config.h"
+
+// void pointing_device_init_user(void) {
+//     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+// }
